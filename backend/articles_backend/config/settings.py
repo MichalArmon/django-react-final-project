cb6bbdc18d81e9ba9566059439e92aecb0f9ef24
Michal <armonmic22003@gmail.com>
2026-06-26 09:28:00 +0300

    c

--- backend/articles_backend/config/settings.py
+++ backend/articles_backend/config/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     "rest_framework",
     "api",
     "django.contrib.admin",
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -128,3 +130,6 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
