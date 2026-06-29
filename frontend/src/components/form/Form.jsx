69778daed5e80293b88cc0b7c80887602fbe02cd
Michal <armonmic22003@gmail.com>
2026-06-29 08:58:50 +0300

    v

--- frontend/src/components/form/Form.jsx
+++ frontend/src/components/form/Form.jsx
@@ -24,11 +24,46 @@ function Form({
     <Box
       component="form"
       color={color}
-      sx={{ mt: 2, p: { xs: 1, sm: 2 }, ...styles }}
+      sx={{ width: "100%", mt: 2, p: { xs: 1, sm: 2 }, ...styles }}
       onSubmit={onSubmit}
       autoComplete="off"
       noValidate
-    ></Box>
+    >
+      <Typography
+        variant="h3"
+        sx={{
+          fontWeight: 700,
+          mb: 1,
+          color: "text.primary",
+        }}
+      >
+        Register
+      </Typography>
+
+      <Typography
+        sx={{
+          color: "text.secondary",
+          mb: 4,
+        }}
+      >
+        Discover the newest stories, insights and updates.
+      </Typography>
+      <Grid container spacing={spacing}>
+        {children}
+      </Grid>
+      <Grid container spacing={1} my={2} direction="row" width="100%">
+        <Grid item xs={12} sm={6}>
+          {" "}
+          <Button
+            node="cencel"
+            color="error"
+            component="div"
+            variant="outlined"
+            onClick={() => navigate(to)}
+          ></Button>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
