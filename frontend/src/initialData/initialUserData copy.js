34f5bead0af98b069ccfbc67f443d6c36e3bcb61
michal <armonmic22003@gmail.com>
2026-07-06 14:20:07 +0300

    ב

---
+++ frontend/src/initialData/initialUserData copy.js
@@ -0,0 +1,18 @@
+const initialUserData = { firstName: "", lastName: "" };
+
+export default initialUserData;
+
+// {
+//     "id": 14,
+//     "username": "michal124",
+//     "email": "michal@example.com",
+//     "first_name": "Michal",
+//     "last_name": "Armon",
+//     "profile": {
+//         "bio": "Frontend and backend developer",
+//         "city": "Lod",
+//         "age": 39,
+//         "experience_years": 2,
+//         "role": "author"
+//     }
+// }
---
+++ frontend/src/models/User copy.jsx
@@ -0,0 +1,27 @@
+import Joi from "joi";
+
+const User = Joi.object({
+  username: Joi.string().min(3).max(30).required(),
+
+  password: Joi.string().min(8).max(100).required(),
+
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required(),
+
+  first_name: Joi.string().min(2).max(30).required(),
+
+  last_name: Joi.string().min(2).max(30).required(),
+
+  bio: Joi.string().min(2).max(300).allow("").optional(),
+
+  city: Joi.string().min(2).max(50).required(),
+
+  age: Joi.number().min(18).max(120).required(),
+
+  experience_years: Joi.number().min(0).max(60).required(),
+
+  role: Joi.string().valid("author", "reader", "admin").required(),
+});
+
+export default User;
