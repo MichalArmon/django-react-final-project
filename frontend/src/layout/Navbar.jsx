1127af2f4dbad040aba5653f1d00acdd5c3a8bea
MichalArmon <armonmic22003@gmail.com>
2026-07-19 18:40:55 +0300

    v

--- frontend/src/layout/Navbar.jsx
+++ frontend/src/layout/Navbar.jsx
@@ -3,10 +3,21 @@ import { AppBar, Box, IconButton, Tooltip, Avatar } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "../providers/UserProvider";
 import SearchBar from "../components/articles/SearchBar";
+import { useState } from "react";
 
 function Navbar() {
   const { user } = useUser();
   const navigate = useNavigate("");
+  const [registerIsOpen, setRegisterIsOpen] = useState(false);
+  const [loginIsOpen, setLoginIsOpen] = useState(false);
+
+  const toggleRegisterOpen = () => {
+    setRegisterIsOpen((prev) => !prev);
+    setLoginIsOpen((prev) => !prev);
+    {
+      registerIsOpen || loginIsOpen ? navigate("/register") : navigate("/");
+    }
+  };
   return (
     <AppBar
       elevation={0}
@@ -47,7 +58,7 @@ function Navbar() {
           <Tooltip title="REGISTER" arrow placement="bottom">
             <IconButton
               aria-label="Register"
-              onClick={() => navigate("/register")}
+              onClick={toggleRegisterOpen}
               sx={{
                 width: 40,
                 height: 40,
